831f22b57edf66f7d0d46eb367511078b52dbf50
Lucas Amorim Maia <lucas.maia@telefonica.com>
2025-10-23 15:11:06 -0300

    Feature: Cadastro de usuarios

--- frontend/src/components/home/ModalCadastro.jsx
+++ frontend/src/components/home/ModalCadastro.jsx
@@ -1,10 +1,61 @@
 import { Modal } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import { Row } from "react-bootstrap";
-import { useForm } from "react-hook-form";
-
+import { Button } from "react-bootstrap";
+import { useForm, useWatch } from "react-hook-form";
+import { FormProvider } from "react-hook-form";
+import CadastroEndereco from "./CadastroEndereco";
+import CadastroPessoa from "./CadastroPessoa";
 function ModalCadastro(props) {
   const metodos = useForm();
+
+  const utilizar = useWatch({
+    control: metodos.control,
+    name: "utilizarIgual",
+  });
+
+  const handleSubmit = async (data) => {
+    const pessoa = {
+      nome: data.nome + " " + data.sobrenome,
+      cpf: data.cpf,
+      data_nascimento: new Date(data.nascimento).toISOString().split("T")[0],
+      genero: data.genero,
+      email: data.email,
+      senha: data.senha,
+      status: "ativo",
+      grupo: "cliente",
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/pessoas", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(pessoa),
+      });
+      if (response.ok) {
+        alert("Usuário adicionado com sucesso");
+
+        const url = `http://localhost:8080/api/pessoas/buscar?email=${encodeURIComponent(
+          data.email
+        )}&senha=${encodeURIComponent(data.senha)}`;
+
+        const resposta = await fetch(url, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const dado = await resposta.json();
+        console.log(dado);
+      }
+    } catch (erro) {
+      console.log(erro.message);
+    }
+    console.log(pessoa);
+  };
+
   return (
     <Modal show={props.show} onHide={props.onHide} centered size="lg">
       <Modal.Header
@@ -18,145 +69,30 @@ function ModalCadastro(props) {
         className="w-100 text-white d-flex flex-column align-content-center justify-content-center"
         style={{ backgroundColor: "#EDEFF2" }}
       >
-        <Row className="mb-3 d-flex justify-content-center">
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Nome</Form.Label>
-            <Form.Control type="text" required minLength={3} />
-          </Form.Group>
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Sobrenome</Form.Label>
-            <Form.Control required type="text" minLength={3} />
-          </Form.Group>
-        </Row>
-        <Row className="mb-3 d-flex justify-content-center">
-          <Form.Group className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>CPF</Form.Label>
-            <Form.Control
-              type="text"
-              inputMode="numeric"
-              maxLength={11}
-              minLength={11}
-              {...metodos.register("cpf", {
-                required: "Campo obrigatório",
-                pattern: {
-                  value: /^\d+$/,
-                  message: "Apenas números são permitidos",
-                },
-              })}
-              onChange={(e) => {
-                const onlyNums = e.target.value.replace(/\D/g, "");
-                metodos.setValue("cpf", onlyNums);
-              }}
-            />
-          </Form.Group>
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Genêro</Form.Label>
-            <Form.Select>
-              <option value="masculino">Masculino</option>
-              <option value="feminino">Feminino</option>
-              <option value="outro">Outro</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
-        <Row className="mb-5 d-flex justify-content-center">
-          <Form.Group className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>
-              Data de nascimento
-            </Form.Label>
-            <Form.Control type="date" required></Form.Control>
-          </Form.Group>
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Senha</Form.Label>
-            <Form.Control required></Form.Control>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3 d-flex justify-content-center">
-          <Row
-            className="fw-bolder"
-            style={{ width: "83.5%", color: "#34495E" }}
-          >
-            Endereço de faturamento
-          </Row>
-          <Form.Group className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>CEP</Form.Label>
-
-            <Form.Control
-              type="text"
-              inputMode="numeric"
-              maxLength={9}
-              minLength={8}
-              {...metodos.register("cep", {
-                required: "Campo obrigatório",
-                pattern: {
-                  value: /^\d+$/,
-                  message: "Apenas números são permitidos",
-                },
-              })}
-              onChange={(e) => {
-                const onlyNums = e.target.value.replace(/\D/g, "");
-                metodos.setValue("cep", onlyNums);
-              }}
-            />
-          </Form.Group>
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Bairro</Form.Label>
-            <Form.Control required></Form.Control>
-          </Form.Group>
-        </Row>
-        <Row className="mb-3 d-flex justify-content-center">
-          <Form.Group md="4" className="col-7">
-            <Form.Label style={{ color: "#34495E" }}>Lougradouro</Form.Label>
-            <Form.Control type="text" required minLength={3} />
-          </Form.Group>
-          <Form.Group md="4" className="col-3">
-            <Form.Label style={{ color: "#34495E" }}>Número</Form.Label>
-            <Form.Control required type="number" />
-          </Form.Group>
-        </Row>
-        <Row className="mb-3 d-flex justify-content-center">
-          <Form.Group md="4" className="col-5">
-            <Form.Label style={{ color: "#34495E" }}>Complemento</Form.Label>
-            <Form.Control type="text" required minLength={3} />
-          </Form.Group>
-          <Form.Group md="4" className="col-3">
-            <Form.Label style={{ color: "#34495E" }}>Cidade</Form.Label>
-            <Form.Control type="text" required></Form.Control>
-          </Form.Group>
-          <Form.Group md="4" className="col-2">
-            <Form.Label style={{ color: "#34495E" }}>UF</Form.Label>
-            <Form.Select required>
-              <option value="">Selecione</option>
-              <option value="AC">AC</option>
-              <option value="AL">AL</option>
-              <option value="AP">AP</option>
-              <option value="AM">AM</option>
-              <option value="BA">BA</option>
-              <option value="CE">CE</option>
-              <option value="DF">DF</option>
-              <option value="ES">ES</option>
-              <option value="GO">GO</option>
-              <option value="MA">MA</option>
-              <option value="MT">MT</option>
-              <option value="MS">MS</option>
-              <option value="MG">MG</option>
-              <option value="PA">PA</option>
-              <option value="PB">PB</option>
-              <option value="PR">PR</option>
-              <option value="PE">PE</option>
-              <option value="PI">PI</option>
-              <option value="RJ">RJ</option>
-              <option value="RN">RN</option>
-              <option value="RS">RS</option>
-              <option value="RO">RO</option>
-              <option value="RR">RR</option>
-              <option value="SC">SC</option>
-              <option value="SP">SP</option>
-              <option value="SE">SE</option>
-              <option value="TO">TO</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
-        
+        <FormProvider {...metodos}>
+          <Form onSubmit={metodos.handleSubmit(handleSubmit)}>
+            <CadastroPessoa />
+            <CadastroEndereco tipo={"faturamento"} />
+            <Row className="mb-3 d-flex justify-content-center">
+              <Row
+                className="fw-bolder mb-3"
+                style={{ width: "86%", color: "#34495E" }}
+              >
+                <Form.Check
+                  defaultChecked
+                  type="switch"
+                  id="custom-switch"
+                  label="Utilizar o mesmo endereço de faturamento para a entrega"
+                  {...metodos.register("utilizarIgual")}
+                />
+              </Row>
+              {utilizar == false && <CadastroEndereco tipo={"entrega"} />}
+            </Row>
+            <Button type="submit" className="bnt">
+              Enviar
+            </Button>
+          </Form>
+        </FormProvider>
       </Modal.Body>
     </Modal>
   );
